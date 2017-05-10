849ee8bccd180221fc7e6c23cc01f883f9ea66d1
benok <benok1@gmail.com>
2017-05-10 15:45:44 +0900

    * Moved FloatConv related files to "FloatConv" folder. * Added copyright info for FloatConv files.

---
+++ Source/FloatConv/FloatConv.Extended.pas
@@ -0,0 +1,35 @@
+﻿// **************************************************************************************************
+// The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+// ANY KIND, either express or implied. See the License for the specific language governing rights
+// and limitations under the License.
+//
+// The Original Code is FloatConv.Extended.pas.
+//
+// The Initial Developer of the Original Code is benok.
+// Portions created by benok are Copyright (C) 2016-2017 benok
+// All Rights Reserved.
+//
+// **************************************************************************************************
+
+// **************************************************************************************************
+// authors page:  https://github.com/benok/zcontrols
+// (contributed for https://github.com/MahdiSafsafi/zcontrols)
+// **************************************************************************************************
+
+unit FloatConv.Extended;
+
+interface
+
+uses
+  FloatConv;
+
+type  
+  TFloat = Extended;
+
+{$I FloatConv_impl.inc}
+
+end.
