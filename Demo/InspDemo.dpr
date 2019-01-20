e4b23d53772e4c15234e06f4e9418887280b412b
MahdiSafsafi <ismspider@gmail.com>
2019-01-20 22:19:08 +0100

    updated copyright.

--- Demo/InspDemo.dpr
+++ Demo/InspDemo.dpr
@@ -2,7 +2,17 @@ program InspDemo;
 
 uses
   Vcl.Forms,
-  uMain in 'uMain.pas' {Main};
+  uMain in 'uMain.pas' {Main},
+  zBase in '..\Source\zBase.pas',
+  zCanvasStack in '..\Source\zCanvasStack.pas',
+  zControlsReg in '..\Source\zControlsReg.pas',
+  zGraphicDialog in '..\Source\zGraphicDialog.pas' {GraphicDialog},
+  zObjInspDialogs in '..\Source\zObjInspDialogs.pas',
+  zObjInspector in '..\Source\zObjInspector.pas',
+  zObjInspList in '..\Source\zObjInspList.pas',
+  zRecList in '..\Source\zRecList.pas',
+  zStringsDialog in '..\Source\zStringsDialog.pas' {StringsDialog},
+  zUtils in '..\Source\zUtils.pas';
 
 {$R *.res}
 
--- Source/zGraphicDialog.pas
+++ Source/zGraphicDialog.pas
@@ -10,7 +10,7 @@
 // The Original Code is zGraphicDialog.pas.
 //
 // The Initial Developer of the Original Code is Mahdi Safsafi [SMP3].
-// Portions created by Mahdi Safsafi . are Copyright (C) 2013-2018 Mahdi Safsafi .
+// Portions created by Mahdi Safsafi. are Copyright (C) 2013-2019 Mahdi Safsafi.
 // All Rights Reserved.
 //
 // **************************************************************************************************
